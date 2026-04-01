1d49c2b62e882395060080fda2d7788d811dc010
Maxwel Hernandez <maxwelhernandez@gmail.com>
2026-04-01 19:42:39 +0000

    upgrading model, and campos de la tabla

--- src/models.py
+++ src/models.py
@@ -1,60 +1,57 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey, Integer
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(
         String(50), unique=True, nullable=False)
     firstname: Mapped[str] = mapped_column(String(50))
     lastname: Mapped[str] = mapped_column(String(50))
-    phonenumber: Mapped[str] = mapped_column(str(20))
+    phonenumber: Mapped[str] = mapped_column(String(20))
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    # birthday: Mapped[date] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    # Usuarios que ME SIGUEN
+    followers: Mapped[list["Follower"]] = relationship(
+        "Follower",
+        foreign_keys="Follower.following_id",
+        back_populates="following"
+    )
 
-followers: Mapped[list["Follower"]] = relationship(
-    "Follower",
-    foreign_keys="Follower.user_id",
-    back_populates="user"
-)
+    # Usuarios a los que YO SIGO
+    followed: Mapped[list["Follower"]] = relationship(
+        "Follower",
+        foreign_keys="Follower.follower_id",
+        back_populates="follower"
+    )
 
-# Usuarios a los que ESTE usuario sigue
-
-followed: Mapped[list["Follower"]] = relationship(
-    "Follower",
-    foreign_keys="Follower.user_follower_id",
-    back_populates="follower"
-)
-
-
-def serialize(self):
-    return {
-        "id": self.id,
-        "username": self.username,
-        "firstname": self.firstname,
-        "lastname": self.lastname,
-        "email": self.email,
-        #   "birthday": self.birthday.isoformat(),
-        "is_active": self.is_active
-    }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            "is_active": self.is_active
+        }
 
 
 class Follower(db.Model):
     __tablename__ = "followers"
 
-    user_id: Mapped[int] = mapped_column(
+    follower_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
         primary_key=True
     )
-    user_follower_id: Mapped[int] = mapped_column(
+    following_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
         primary_key=True
     )
@@ -62,13 +59,13 @@ class Follower(db.Model):
     # Usuario seguido
     user: Mapped["User"] = relationship(
         "User",
-        foreign_keys=[user_id],
+        foreign_keys=[follower_id],
         back_populates="followers"
     )
 
     # Usuario que sigue
     follower: Mapped["User"] = relationship(
         "User",
-        foreign_keys=[user_follower_id],
+        foreign_keys=[following_id],
         back_populates="followed"
     )
